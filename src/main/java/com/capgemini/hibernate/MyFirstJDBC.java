169a552b1c3aff634764ee0ddf0b9f3057a9d999
suyash <suyash@suyash-PC>
2019-02-08 11:55:36 +0530

    .

--- src/main/java/com/capgemini/hibernate/MyFirstJDBC.java
+++ src/main/java/com/capgemini/hibernate/MyFirstJDBC.java
@@ -1,5 +1,17 @@
 package com.capgemini.hibernate;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class MyFirstJDBC {
 
+	public static void main(String[] args) {
+		
+		
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+	}
 }
