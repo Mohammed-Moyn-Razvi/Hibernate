ff4cc472baf02738ae649451fe3034f3a88b04fc
suyash <suyash@suyash-PC>
2019-02-08 12:25:42 +0530

    configuration

---
+++ src/main/java/com/capgemini/hibernate/Employee.java
@@ -0,0 +1,50 @@
+package com.capgemini.hibernate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Emp")
+public class Employee {
+
+	@Id
+	@Column(name="E_id")
+	private int e_id;
+	@Column(name="E_name")
+	private String e_name;
+	@Column(name="E_sal")
+	private double e_salary;
+	@Column(name="E_pass")
+	private String e_password;
+	public int getE_id() {
+		return e_id;
+	}
+	public void setE_id(int e_id) {
+		this.e_id = e_id;
+	}
+	public String getE_name() {
+		return e_name;
+	}
+	public void setE_name(String e_name) {
+		this.e_name = e_name;
+	}
+	public double getE_salary() {
+		return e_salary;
+	}
+	public void setE_salary(double e_salary) {
+		this.e_salary = e_salary;
+	}
+	public String getE_password() {
+		return e_password;
+	}
+	public void setE_password(String e_password) {
+		this.e_password = e_password;
+	}
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/com/capgemini/hibernate/TestEmp.java
@@ -0,0 +1,31 @@
+package com.capgemini.hibernate;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class TestEmp {
+public static void main(String[] args) {
+	
+	
+	EntityManagerFactory emf = new Persistence().createEntityManagerFactory("myPersistanceUnit");
+	EntityManager manager = emf.createEntityManager();
+	manager.getTransaction().begin();
+	
+	Employee emp = new Employee();
+	emp.setE_id(101);
+	emp.setE_name("Dinga");
+	emp.setE_salary(9000);
+	emp.setE_password("root");
+	
+	manager.persist(emp);
+	
+	
+	manager.getTransaction().commit();
+	manager.close();
+	
+	
+	
+	
+}
+}
